30c5b2364e752ebfc7c92d498512bfadcf2050bc
CtrlAltDelete4413 <95325881+CtrlAltDelete4413@users.noreply.github.com>
2025-01-26 22:15:50 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'sobing4413'
+
+version '1.0'
+
+client_script {
+  "client.lua",
+}
+
+export "GetClosestNPC"
+export "IsPedNearCoords"
+export "isPed"
+export "GroupRank"
+export "TargetVehicle"
